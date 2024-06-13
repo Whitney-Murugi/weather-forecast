5768fc97140391cd2ed77b9dda82d57d9642e189
Whitney-Murugi <mmaitsimurugi@gmail.com>
2024-06-13 19:28:20 +0300

    Added a weather API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+function giveResponse(response) {
+  let area = document.querySelector("#Weather");
+  let data = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+
+  city.innerHTML = response.data.city;
+  area.innerHTML = data;
+}
+
+function getTemperature(city) {
+  let ApiKey = "23e2742eef1f1bc7tod430e337aaf4bd";
+  let ApiCall = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${ApiKey}&units=metric`;
+  axios.get(ApiCall).then(giveResponse);
+}
+
 function searchCity(event) {
   event.preventDefault();
-
   let input = document.querySelector("#search");
-  let city = document.querySelector("#city");
-  city.innerHTML = input.value;
+  getTemperature(input.value);
 }
 
 let searchElement = document.querySelector("#search-form");
